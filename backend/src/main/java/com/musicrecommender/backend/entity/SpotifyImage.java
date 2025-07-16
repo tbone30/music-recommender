b5218523d4ef80b93f17c6ad96de8f49d00403c8
tbone30 <twelch8585@gmail.com>
2025-07-16 12:48:28 -0400

    SpotifyImage entity

---
+++ backend/src/main/java/com/musicrecommender/backend/entity/SpotifyImage.java
@@ -0,0 +1,41 @@
+package com.musicrecommender.backend.entity;
+
+public class SpotifyImage {
+    private String url;
+    private int width;
+    private int height;
+
+    public SpotifyImage() {
+        // Default constructor for JPA
+    }
+
+    public SpotifyImage(String url, int width, int height) {
+        this.url = url;
+        this.width = width;
+        this.height = height;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
