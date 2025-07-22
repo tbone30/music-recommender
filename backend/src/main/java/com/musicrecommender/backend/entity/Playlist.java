e0c0083d6a375fa09ba756f50b70d5f6da4cc42f
tbone30 <twelch8585@gmail.com>
2025-07-22 11:02:27 -0400

    playlist entity

--- backend/src/main/java/com/musicrecommender/backend/entity/Playlist.java
+++ backend/src/main/java/com/musicrecommender/backend/entity/Playlist.java
@@ -26,5 +26,91 @@ public class Playlist {
         // Default constructor for JPA
     }
 
-    
+    public Boolean getCollaborative() {
+        return collaborative;
+    }
+
+    public void setCollaborative(Boolean collaborative) {
+        this.collaborative = collaborative;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<SpotifyImage> getImages() {
+        return images;
+    }
+
+    public void setImages(List<SpotifyImage> images) {
+        this.images = images;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getOwnerDisplayName() {
+        return ownerDisplayName;
+    }
+
+    public void setOwnerDisplayName(String ownerDisplayName) {
+        this.ownerDisplayName = ownerDisplayName;
+    }
+
+    public String getIsPublic() {
+        return isPublic;
+    }
+
+    public void setIsPublic(String isPublic) {
+        this.isPublic = isPublic;
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
 }
